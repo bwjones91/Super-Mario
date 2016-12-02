9116df539594b59e0518b2b3ab011febb6e5b726
bwjones91 <bwjones1991@gmail.com>
2016-12-02 14:16:53 -0500

    Castle Animation and Mario Animation tweaks

---
+++ Super Mario/Assets/Castle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Castle : MonoBehaviour {
+    
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        anim.SetTrigger("End Level");
+    }
+
+}
